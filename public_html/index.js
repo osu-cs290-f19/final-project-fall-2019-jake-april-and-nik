276d9dede98835d573873a13f39e5e3ac3ee7bb7
achatzn <56105294+achatzn@users.noreply.github.com>
2019-12-03 21:53:10 -0800

    Cleaned up the website a lot
    Added hamburger menu with some basic option including a beautiful dark mode. Adjusted other areas of the website for dark mode.

--- public_html/index.js
+++ public_html/index.js
@@ -1,1 +1,14 @@
-console.log("swag");
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+function darkMode(){
+
+    document.getElementById("body").classList.toggle("dark_mode");
+    document.getElementById("site-user-info").classList.toggle("dark_mode_name");
+    document.getElementById("dark-mode-user").classList.toggle("dark_mode_label");
+    document.getElementById("dark-mode-user-name").classList.toggle("dark_mode_label");
+
+}
