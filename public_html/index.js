eb3b2894a72615cfa5226e34824d06c37e03f0e0
achatzn <56105294+achatzn@users.noreply.github.com>
2019-12-05 12:15:26 -0800

    Added some basic javascript to make messaging happen

--- public_html/index.js
+++ public_html/index.js
@@ -1,3 +1,8 @@
+var newPost = document.getElementById("send");
+var msgToSend = document.getElementById("chatbox-id");
+var posts = document.getElementsByClassName("message")
+var postsContainer = document.getElementById("message-display");
+
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");
@@ -11,5 +16,53 @@ function darkMode(){
     document.getElementById("dark-mode-user").classList.toggle("dark_mode_label");
     document.getElementById("dark-mode-user-name").classList.toggle("dark_mode_label");
     document.getElementById("dark-mode-send").classList.toggle("dark_mode_label");
+    
+}
 
+newPost.addEventListener('click', function (event) {
+    console.log("== button pressed, should post");
+    posts = document.getElementsByClassName("message");
+    if(msgToSend.value.length > 200){
+        alert("Messages are limited to 200 characters!");
+    }else{
+        if(posts.length > 3)
+            posts[0].remove();
+        insertNewPost(msgToSend.value);
+    }
+    event.stopPropagation();
+});
+
+/// INSERT NEW MESSAGE BASIC NEEDS TEMPLATE
+
+function insertNewPost(m) {
+  var contents = document.createElement('div');
+  contents.classList.add("message");
+    
+  var content = document.createElement('div');
+  content.classList.add("message-content");
+  contents.appendChild(content);
+    
+  var postPhotoImg = document.createElement('div');
+  postPhotoImg.classList.add("message-profile-picture");
+  content.appendChild(postPhotoImg);
+    
+  var name = document.createElement('p');
+  name.classList.add("username");
+  name.textContent = "Anon";
+  postPhotoImg.appendChild(name);
+    
+  var image = document.createElement('img');
+  image.src="https://howfix.net/wp-content/uploads/2018/02/sIaRmaFSMfrw8QJIBAa8mA-article.png";
+  postPhotoImg.appendChild(image);
+
+  var info = document.createElement('div');
+  info.classList.add("message-info-holder");
+  content.appendChild(info);
+    
+  var msg = document.createElement('p');
+  msg.id = "post-text";
+  msg.textContent = m;
+  info.appendChild(msg);
+    
+  postsContainer.appendChild(contents);
 }
